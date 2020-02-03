181b87c728568ff191210cb7ccd0c4ec38c21302
Mirian Iosimura <mirian.iosimura@gmail.com>
2020-02-03 11:57:41 -0300

    refactoring methods in characters_controller

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,26 +1,45 @@
 class Api::V1::CharactersController < ActionController::API
   def show
-    id = params[:id]
-    response_character = Faraday.get "https://rickandmortyapi.com/api/character/#{id}"
+    character = response_character(params[:id])
 
-    if response_character.status == 200
-      name = JSON.parse(response_character.body)["name"]
-      episode = JSON.parse(response_character.body)["episode"].first
+    if character.status == 200
+      name = find_name(character)
+      first_episode_url = find_url_first_episode(character)
+      episode = response_episode(first_episode_url)
+      air_date = find_air_date(episode)
       
-      response_episode = Faraday.get episode
-      air_date = JSON.parse(response_episode.body)["air_date"]
-
       render json: {
         name: name,
         air_date: air_date
       }, status: :ok
-    
-    elsif response_character.status == 404
-      render json: response_character.body, status: :not_found
-
-    elsif response_character.status == 500
-      render json: response_character.body, status: :internal_server_error
-
+        
+    elsif character.status == 404
+      render json: character.body, status: :not_found
+      
+    elsif character.status == 500
+      render json: character.body, status: :internal_server_error
+        
     end
+  end
+  
+  private
+  def response_character(id)
+    response = Faraday.get "https://rickandmortyapi.com/api/character/#{id}"
+  end
+  
+  def find_name(character)
+    name = JSON.parse(character.body)["name"]
+  end
+
+  def find_url_first_episode(character)
+    episode = JSON.parse(character.body)["episode"].first
+  end
+
+  def response_episode(url)
+    response_episode = Faraday.get url
+  end
+
+  def find_air_date(episode)
+    air_date = JSON.parse(episode.body)["air_date"]
   end
 end
